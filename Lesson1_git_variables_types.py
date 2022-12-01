4e0486ec55a7d0de423097269513c2e913a7dccf
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-12-01 21:21:14 +0200

    Переменніе, именование

--- Lesson1_git_variables_types.py
+++ Lesson1_git_variables_types.py
@@ -1,1 +1,9 @@
-print('hello')
+variable = 87
+print(variable)
+variable = 'Taras'
+print(variable)
+
+apples_in_box = 98  # snake_case
+applesInBox = 74  # camelCase
+# Ctrl+Alt+L <- автоматическое форматирование кода по pep8
+
