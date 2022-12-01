2bf0679b1abfb9b0a909f92328dfa6d14d8adee4
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-12-01 21:30:29 +0200

    типы данных

--- Lesson1_git_variables_types.py
+++ Lesson1_git_variables_types.py
@@ -7,3 +7,7 @@ apples_in_box = 98  # snake_case
 applesInBox = 74  # camelCase
 # Ctrl+Alt+L <- автоматическое форматирование кода по pep8
 
+i = int()  # целые числа
+f = float()  # дробные числа (разделяются точкой)
+s = str()  # строки
+b = bool  # логические данные (истина/ложь(True/False))
