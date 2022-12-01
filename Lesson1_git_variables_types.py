0fdbb00b16bf015ec4f77b5986143d7e0656cc63
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-12-01 22:09:16 +0200

    форматирование строк

--- Lesson1_git_variables_types.py
+++ Lesson1_git_variables_types.py
@@ -11,3 +11,24 @@ i = int()  # целые числа
 f = float()  # дробные числа (разделяются точкой)
 s = str()  # строки
 b = bool  # логические данные (истина/ложь(True/False))
+
+some_string = 'очень важная строка'
+print(some_string)
+some_string = "очень важная строка с кавычко'й в случайном месте"
+print(some_string)
+some_string = 'очень важная строка с кавычко\'й в случайном месте'
+print(some_string)
+some_string = "очень важная строка с кавычко\"й в случайном месте"
+print(some_string)
+some_string = '"очень важная строка \nнаписанная в две линии"'
+print(some_string)
+some_string = '''очень важная строка 
+написанная в две линии'''
+print(some_string)
+some_string = '"очень важная строка с табул\tяцией в слове табуляц\tия"'
+print(some_string)
+
+print('a\ta')
+print('aa\ta')
+print('aaa\ta')
+print('aaaa\ta')
