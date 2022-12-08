3f079c6595852cfcf43dda8b2928e795f0741c45
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-12-08 21:09:08 +0200

    операторы

--- Lesson2_operators_f-string_in-out.py
+++ Lesson2_operators_f-string_in-out.py
@@ -1,20 +1,99 @@
-i = int()
-f = float()
-s = str()
-b = bool()
+# i = int()
+# f = float()
+# s = str()
+# b = bool()
+#
+# i = 123
+# s = '345'
+# # new_string = s + i
+# new_string = s + str(i)
+# print(new_string)
+#
+# num_1 = 123
+# num_2 = 2.24
+# result = num_1 + num_2
+# print(type(result), result)
+# next_result=int(result)
+# print(type(next_result), next_result)
+#
+# new_string = int(s) + i
+# print(new_string)
 
-i = 123
-s = '345'
-# new_string = s + i
-new_string = s + str(i)
-print(new_string)
+'''
+операторы
+арифметические
++
+-
+*
+/
+%   - получение остатка от деления
+//  - деление нацело
+**  - возведение в степень
+=   - присвоение
+        (справа налево, т.е.
+        a = 5   пятерка записалась в a
+        b = 7   семерка записалась в b
+        a = b   семерка записалась в a)
++=  - a = a + b эквивалентно a += b
+-=
+*=
+/=
+'''
 
-num_1 = 123
-num_2 = 2.24
-result = num_1 + num_2
-print(type(result), result)
-next_result=int(result)
-print(type(next_result), next_result)
+'''
+логические
+>
+<
+>=
+<=
+==  - равно
+!=  - не равно
+and - и
+    1 and 1 -> 1
+    1 and 0 -> 0
+    0 and 1 -> 0
+    0 and 0 -> 0
 
-new_string = int(s) + i
-print(new_string)
+or  - или
+    1 or 1 -> 1
+    1 or 0 -> 1
+    0 or 1 -> 1
+    0 or 0 -> 0
+
+^   - исключающее или (XOR)
+    1 ^ 1 -> 0
+    1 ^ 0 -> 1
+    0 ^ 1 -> 1
+    0 ^ 0 -> 0
+
+not - инверсия bool-значения
+
+'''
+print(7 > 5)
+print(not 7 > 5)
+print(7 > 5 and 7 > 12)
+print(7 > 5 or 7 > 12)
+print(7 > 5 ^ 7 > 12)
+print(7 != 5)
+# print(7 not 5)
+print(not 7 == 5)
+'''
+тождественности
+is      - идентичность операндов(участники операции)
+is not  - не идентичность операндов
+'''
+a = 123
+b = 123.0
+print(a == b)
+print(a is b)
+
+'''
+принадлежности
+in      - принадлежит
+not in  - наоборот
+'''
+some_string = 'sinchrophazotron'
+print('a' in some_string)
+print('a' not in some_string)
+some_another_string = 'Мама мыла раму'
+print('мыла' in some_another_string)
