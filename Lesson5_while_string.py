9878f9c34ac969fb2c1fee6450869c977e8b8621
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-12-19 21:00:33 +0200

    цикл в цикле

--- Lesson5_while_string.py
+++ Lesson5_while_string.py
@@ -37,9 +37,31 @@ while выражение:
 #     print('NO WELCOME')
 
 # получить сумму всех цифр числа
-num = int(input('Input number: '))
-result = 0
-while num > 0:
-    result += num % 10  # result = result + num % 10
-    num //= 10  # num = num // 10
-print(result)
+# num = int(input('Input number: '))
+# result = 0
+# while num > 0:
+#     result += num % 10  # result = result + num % 10
+#     num //= 10  # num = num // 10
+# print(result)
+
+# цикл в цикле
+# sym_in_string = 5
+# count_of_strings = 4
+# for h in range(count_of_strings):
+#     for w in range(sym_in_string):
+#         print('*', end=' ')
+#     print()
+
+# columns = 5
+# strings = 5
+# for s in range(strings):
+#     for c in range(columns):
+#         print('*', end=' ')
+#     print()
+
+columns = 5
+strings = 5
+for s in range(strings):
+    for c in range(s + 1):
+        print('*', end=' ')
+    print()
