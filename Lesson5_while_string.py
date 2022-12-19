5563b8d9b14f0b47939711189c655be7449bac27
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-12-19 22:00:56 +0200

    функции строк

--- Lesson5_while_string.py
+++ Lesson5_while_string.py
@@ -69,23 +69,53 @@ while выражение:
 строка - НЕИЗМЕНЯЕМЫЙ тип данных, представляет собой последовательность символов
 является итерируемым объектом
 '''
-my_string = 'string'
-my_string2 = str('another string')
-my_string3 = '''multiline
-string'''
-print('len(my_string)', len(my_string))  # len() - функция возвращающая длину итерируемого объекта
-print('my_string[0]', my_string[0])  # обращение к символу по индексу
-print('my_string[-1]', my_string[-1])  # отрицательные индексы обращаются с конца
-'''срезы'''
-# string[старт:стоп:шаг] default[0:len(string):1]
-print('string[старт:стоп:шаг] default[0:len(string):1]')
-print('my_string[0:len(my_string):1]', my_string[0:len(my_string):1])
-print('my_string[2:]', my_string[2:])
-print('my_string[:4]', my_string[:4])
-print('my_string[1:4]', my_string[1:4])
-print('my_string[4:1:-1]', my_string[4:1:-1])
-print('my_string[::-1]', my_string[::-1])
-# переворот числа
-number = 123456
-#        преобразование        преобразование  переворот
-result =    int                 (str(number)    [::-1])
+# my_string = 'string'
+# my_string2 = str('another string')
+# my_string3 = '''multiline
+# string'''
+# print('len(my_string)', len(my_string))  # len() - функция возвращающая длину итерируемого объекта
+# print('my_string[0]', my_string[0])  # обращение к символу по индексу
+# print('my_string[-1]', my_string[-1])  # отрицательные индексы обращаются с конца
+# '''срезы'''
+# # string[старт:стоп:шаг] default[0:len(string):1]
+# print('string[старт:стоп:шаг] default[0:len(string):1]')
+# print('my_string[0:len(my_string):1]', my_string[0:len(my_string):1])
+# print('my_string[2:]', my_string[2:])
+# print('my_string[:4]', my_string[:4])
+# print('my_string[1:4]', my_string[1:4])
+# print('my_string[4:1:-1]', my_string[4:1:-1])
+# print('my_string[::-1]', my_string[::-1])
+# # переворот числа
+# number = 123456
+# #        преобразование        преобразование  переворот
+# result =    int                 (str(number)    [::-1])
+# print(result)
+
+my_string = 'some new string'
+print('my_string ->', my_string)
+
+print('my_string.split() ->', my_string.split())
+print('my_string.split(sep="s") ->', my_string.split(sep="s"))
+print('my_string.upper() ->', my_string.upper())
+print('my_string ->', my_string)
+
+symbol = '.'
+print('symbol.isdigit() ->', symbol.isdigit())  # число
+print('symbol.isalpha() ->', symbol.isalpha())  # буква
+print('symbol.isalnum ->', symbol.isalnum())  # число или буква
+
+text = 'London is the capital of Great Britain'
+print(text)
+print('text.find("a")', text.find("a"))
+print('text.find("ap")', text.find("ap"))
+print('text.find("al")', text.find("al"))
+
+print("text.replace('London', 'Kyiv')", text.replace('London', 'Kyiv'))
+print(text)
+print("text.replace('Great Britain', '***')", text.replace('Great Britain', '***'))
+
+# родитель f-строк
+print('{}, {}, {}')
+print('{}, {}, {}'.format(12, 'Stone', 'table'))
+print('{1}, {2}, {0}'.format(12, 'Stone', 'table'))
+print('{c}, {a}, {b}'.format(a=12, b='Stone', c='table'))
