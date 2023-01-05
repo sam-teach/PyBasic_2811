3d544af7078f17193ab0b9a726cf9cefa9e08ced
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2023-01-05 21:39:25 +0200

    dictionary вхождение в словарь

--- Lesson9_dictionary.py
+++ Lesson9_dictionary.py
@@ -48,58 +48,74 @@
 # my_dict = {'apple': {'eng': 'apple', 'som': 'tufax', 'de': 'apfel', 'fr': 'pomme'}}
 # print(my_dict['apple']['eng'])
 # print(my_dict['apple']['fr'])
+# '''
+# методы словарей
+# '''
+#
+# address = {
+#     'city': 'Kyiv',
+#     'street': 'Lesya Kurbasa',
+#     'house': 25,
+#     'apartment': 33,
+#     14: 'четырнадцать'
+# }
+# person = {
+#     'name': 'Volodimir',
+#     'age': 42,
+#     'address': address,
+# }
+# print(person)
+# print(person['name'])
+# # print(person['job']) # KeyError: 'job'
+# print(person.get('job'))  # позволяет получить значение по ключу, в случае отсутствия ключа вернет None, а не ошибку
+# #  добавление одного элемента в словарь
+# person['job'] = 'president'
+# print(person['job'])
+# print(person)
+# print(person.keys())  # возвращает ключи словаря
+# for key in person.keys():
+#     print(key)
+#
+# print(person.values())  # возвращает значения словаря
+# for value in person.values():
+#     print(value)
+#
+# for i in person:  # при переборе словаря в цикле, перебираться будут ключи
+#     print(i, end='-')
+#
+# print(person.items())
+# for element in person.items():
+#     print(element, end=' - ')
+# print()
+# print('-------------')
+# # метод добавления нескольких элементов в словарь
+# person.update({'legs': 2, 'hands': 2, 'head': 1})
+# print(person)
+# # удаление элемента
+# del (person['age'])
+# print(person)
+# # del (person['age'])  # KeyError: 'age' <- попытка удаления несуществующего ключа
+# print(person.pop('legs'))  # KeyError: 'age' <- попытка удаления несуществующего ключа
+# print(person)
+# # dictionary.pop( key, d) - удаляет элемент с ключом key и возвращает его значение,
+# # если такого ключа нет вернет d,
+# # если d не передавали в метод вызовет ошибку
+# print(person.pop('legs', None))
+# li = list(person)
+# print(li)
+
 '''
-методы словарей
+проверка на вхождение в словарь
 '''
-
-address = {
-    'city': 'Kyiv',
-    'street': 'Lesya Kurbasa',
-    'house': 25,
-    'apartment': 33,
-    14: 'четырнадцать'
+cube_dict = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six'
 }
-person = {
-    'name': 'Volodimir',
-    'age': 42,
-    'address': address,
-}
-print(person)
-print(person['name'])
-# print(person['job']) # KeyError: 'job'
-print(person.get('job'))  # позволяет получить значение по ключу, в случае отсутствия ключа вернет None, а не ошибку
-#  добавление одного элемента в словарь
-person['job'] = 'president'
-print(person['job'])
-print(person)
-print(person.keys())  # возвращает ключи словаря
-for key in person.keys():
-    print(key)
-
-print(person.values())  # возвращает значения словаря
-for value in person.values():
-    print(value)
-
-for i in person:  # при переборе словаря в цикле, перебираться будут ключи
-    print(i, end='-')
-
-print(person.items())
-for element in person.items():
-    print(element, end=' - ')
-print()
-print('-------------')
-# метод добавления нескольких элементов в словарь
-person.update({'legs': 2, 'hands': 2, 'head': 1})
-print(person)
-# удаление элемента
-del (person['age'])
-print(person)
-# del (person['age'])  # KeyError: 'age' <- попытка удаления несуществующего ключа
-print(person.pop('legs'))  # KeyError: 'age' <- попытка удаления несуществующего ключа
-print(person)
-# dictionary.pop( key, d) - удаляет элемент с ключом key и возвращает его значение,
-# если такого ключа нет вернет d,
-# если d не передавали в метод вызовет ошибку
-print(person.pop('legs', None))
-li = list(person)
-print(li)
+print(2 in cube_dict)
+print(2 in cube_dict.keys())
+print('two' in cube_dict)
+print('two' in cube_dict.values())
