c1cca2a96d60cb60a64c9fb221fc5870754fccab
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2023-01-09 21:05:38 +0200

    functions аргументы по умолчанию, именованные

--- Lesson10_functions_modules.py
+++ Lesson10_functions_modules.py
@@ -1,47 +1,81 @@
+# '''
+# функции
+# def имя_функции(аргумент(ы)):
+#     """строка документации"""
+#     тело функции(операторы)(код)
+# '''
+#
+#
+# def function_name():
+#     """some strings with describe function"""
+#     pass
+#
+#
+# function_name()
+# print(function_name.__doc__)
+#
+#
+# def print_hello_message() -> None:
+#     """
+#     printing hello message
+#     :return:
+#     """
+#     print('Hello!')
+#
+#
+# def print_named_hello_message(name: str) -> None:
+#     """
+#     printing named hello message
+#     :param name:
+#     :return:
+#     """
+#     print(f'Hell {name}!')
+#
+#
+# def calc(num1, op: str, num2) -> float:
+#     """
+#     comments
+#     :param num1: int
+#     :param op:
+#     :param num2:
+#     :return: result
+#     """
+#
+#     return eval(f'{num1}{op}{num2}')
+#
+#
+# result = calc(2, 2, 5)
+# print(result)
+#
+#
+# # calc()
+#
+# def test_fun(b):
+#     print('in fun', b)
+#     b *= 2
+#     print('in fun', b)
+#
+#
+# a = 3
+# test_fun(a)
+# print('out fun', a)
+
 '''
-функции
-def имя_функции(аргумент(ы)):
-    """строка документации"""
-    тело функции(операторы)(код)
+аргументы функций по умолчанию
 '''
 
 
-def function_name():
-    """some strings with describe function"""
-    pass
+def test_fun(arg1='Default_arg1', arg2='Default_arg2') -> None:
+    print(arg1, arg2)
 
 
-function_name()
-print(function_name.__doc__)
+test_fun(1, 2)
+test_fun(1)
+test_fun()
+
+'''
+именованные аргументы
+'''
+test_fun(arg2=123)
 
 
-def print_hello_message():
-    """
-    printing hello message
-    :return:
-    """
-    print('Hello!')
-
-
-def print_named_hello_message(name):
-    """
-    printing named hello message
-    :param name:
-    :return:
-    """
-    print(f'Hell {name}!')
-
-
-def calc(num1, op, num2):
-    """
-    comments
-    :param num1:
-    :param op:
-    :param num2:
-    :return:
-    """
-    return eval(f'{num1}{op}{num2}')
-
-
-result = calc(2, '+', 5)
-print(result)
